402051304b50a88c3556f5f48c3b8f08e83b21fc
DanielTrajcov <danieltrajcov@gmail.com>
2024-12-04 06:37:30 +0100

    Commit

--- index.php
+++ index.php
@@ -11,11 +11,6 @@ $user = new User($db);
 $vote = new Vote($db);
 $category = new Category($db);
 
-$defaultSection = 'winners';
-if (!isset($_GET['winners']) && !isset($_GET['vote'])) {
-    header("Location: ?$defaultSection");
-    exit;
-}
 
 $voteResults = null;
 $mostFrequentVoters = null;
@@ -61,7 +56,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['ajax'])) {
 <nav class="text-2xl">
   <div class="max-w-7xl mx-auto px-2 sm:px-8 lg:px-6">
     <div class="relative flex items-center justify-between h-16 ">
-        <a href="#" class="text-white  text-3xl font-bold">
+        <a href="index.php" class="text-white  text-3xl font-bold">
           Vote<span class="accent">Hub</span>
         </a>
 
@@ -289,7 +284,20 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['ajax'])) {
 
     <?php
         }
-    }
+    }else {
     ?>
+    <section>
+    <div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
+        <h1 class="mb-4 text-4xl font-extrabold md:text-5xl lg:text-6xl accent">Who doesn't love a little friendly competition?</h1>
+        <p class="mb-8 text-lg font-normal text-gray-400 lg:text-xl sm:px-16 xl:px-48 ">Vote for your colleagues, leave a comment, and let the best shine!</p>
+        <div class="flex flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
+            <a href="?vote" class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg border border-gray-700">
+                <svg class="mr-2 -ml-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2 6a2 2 0 012-2h6a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V6zM14.553 7.106A1 1 0 0014 8v4a1 1 0 00.553.894l2 1A1 1 0 0018 13V7a1 1 0 00-1.447-.894l-2 1z"></path></svg>
+                Vote
+            </a>  
+        </div>
+    </div>
+</section>
+<?php }?>
 </body>
 </html>
